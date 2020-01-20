407914ce9d6783b8b2e736a966b0908ae49007e6
Alex <aledavydkin@gmail.com>
2020-01-20 22:57:54 +0500

    Вторая задача

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -4,3 +4,8 @@ let physics = 4;
 
 let average = (algebra + geography + physics) / 3;
 console.log('Cредняя оценка за три предмета ' + average);
+
+//Задача Номер 2
+let myName = Alex;
+let massage = `Привет, мир! Меня зовут ${Alex}`;
+console.log(massage);
