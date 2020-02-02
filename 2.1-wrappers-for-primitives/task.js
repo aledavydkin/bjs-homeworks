b49b58c835fd63cae883d50f754586e17a2a4c6e
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-02-02 18:45:22 +0500

    Первое задание Обертка для примитивов

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,4 @@
+'use script'
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -11,8 +12,30 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    const obj = {
+        percent,
+        contribution,
+        amount,
+    }
+
+    for (let prop in obj) {
+        if (Number.isNaN(parseInt(obj[prop]))){
+            console.log(`Параметр ${prop} содержит неправильное значение ${obj[prop]}`);
+            return null;
+        } else {
+            let today = new Date();
+            let dateEnd = new Date(date);
+            let p = percent / 100 / 12;
+            let s = amount - contribution;
+            let n = parseInt((dateEnd - today) / (1000 * 3600 * 24 * 30));
+            let summ = (s * (p + p / (((1 + p) ** n) - 1))).toFixed(2);
+
+            totalAmount = (summ * n).toFixed(2);
+
+            console.log(totalAmount);
+            return totalAmount;
+        }
+    }
 }
 
 function sayHello() {
